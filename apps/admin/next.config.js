924bffe15aa4179111283d0bb33b74f6283b84fc
Rohit <rohit.bhattarai0100@gmail.com>
2026-06-06 15:20:57 +1000

    Vercel Fixes

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,5 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+
+  transpilePackages: ["@repo/auth", "@repo/db", "@soundwave/ui"],
+
   images: {
     remotePatterns: [
       {
